f7aeb8dd4900383594101a26cedafd94fc8870b0
beeinger <sulecki.maciek@gmail.com>
2019-10-25 21:14:02 +0200

    finished boiler example

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import session from "./session";
 import user from "./user";
 import message from "./message";
-import facebook from "./facebook";
 
 export default {
   session,
   user,
-  message,
-  facebook
+  message
 };
